9c28d5aaa7801dd64f27761a851fc1dfb2a4698d
KarinaStabile <karina.stabile@gmail.com>
2022-02-20 16:51:51 -0500

    Completed assignment

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Login() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <input type="password" name="password" placeholder="Password"/>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+  export default Login;
